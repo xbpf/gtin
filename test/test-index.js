67324c4412c574973c807b5d4930dd1daaf23b6b
Ivan <ivan@sq10.net>
2016-07-12 15:56:40 -0500

    minify(), getFormat(), and getRealFormat()

--- test/test-index.js
+++ test/test-index.js
@@ -55,3 +55,116 @@ test('gtin.validate(barcode) (error)', t => {
     }
   }
 })
+
+test('gtin.minify(barcode) (minified)', t => {
+  t.plan(5)
+
+  const cases = [
+    ['00000012341238', '12341238'],
+    ['0000012341238', '12341238'],
+    ['000012341238', '12341238'],
+    ['12341238', '12341238'],
+    ['01234123412344', '1234123412344']
+  ]
+
+  for (let testCase of cases) {
+    t.is(
+      gtin.minify(testCase[0]),
+      testCase[1]
+    )
+  }
+})
+
+test('gtin.minify(barcode) (error)', t => {
+  t.plan(4)
+
+  const cases = [
+    [123, 'Barcode must be a string'],
+    ['abc', 'Barcode is not of a valid format'],
+    ['123', 'Barcode is not of a valid format'],
+    ['001234123412344', 'Barcode is not of a valid format']
+  ]
+
+  for (let testCase of cases) {
+    try {
+      gtin.minify(testCase[0])
+      t.fail('Error not thrown')
+    } catch (err) {
+      t.is(err.message, testCase[1])
+    }
+  }
+})
+
+test('gtin.getFormat(barcode) (ok)', t => {
+  t.plan(4)
+
+  const cases = [
+    ['12341238', 'GTIN-8'],
+    ['123412341234', 'GTIN-12'],
+    ['1234123412344', 'GTIN-13'],
+    ['01234123412344', 'GTIN-14']
+  ]
+
+  for (let testCase of cases) {
+    t.is(
+      gtin.getFormat(testCase[0]),
+      testCase[1]
+    )
+  }
+})
+
+test('gtin.getFormat(barcode) (error)', t => {
+  t.plan(4)
+
+  const cases = [
+    [123, 'Barcode must be a string'],
+    ['abc', 'Barcode is not of a valid format'],
+    ['123', 'Barcode is not of a valid format'],
+    ['123412381', 'Barcode is not of a valid format']
+  ]
+
+  for (let testCase of cases) {
+    try {
+      gtin.getFormat(testCase[0])
+      t.fail('Error not thrown')
+    } catch (err) {
+      t.is(err.message, testCase[1])
+    }
+  }
+})
+
+test('gtin.getRealFormat(barcode) (ok)', t => {
+  t.plan(2)
+
+  const cases = [
+    ['1234123412344', 'GTIN-13'],
+    ['01234123412344', 'GTIN-13']
+  ]
+
+  for (let testCase of cases) {
+    t.is(
+      gtin.getRealFormat(testCase[0]),
+      testCase[1]
+    )
+  }
+})
+
+test('gtin.getRealFormat(barcode) (error)', t => {
+  t.plan(4)
+
+  const cases = [
+    [123, 'Barcode must be a string'],
+    ['abc', 'Barcode is not of a valid format'],
+    ['123', 'Barcode is not of a valid format'],
+    ['123412381', 'Barcode is not of a valid format']
+  ]
+
+  for (let testCase of cases) {
+    try {
+      gtin.getRealFormat(testCase[0])
+      t.fail('Error not thrown')
+    } catch (err) {
+      t.is(err.message, testCase[1])
+    }
+  }
+})
